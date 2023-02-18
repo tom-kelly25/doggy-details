9d332d554e10c17ed1865837afecbdc92592e6af
tom-kelly25 <107217874+tom-kelly25@users.noreply.github.com>
2023-02-18 17:44:05 +0000

    dev 17:43 18/02

--- doggy_details/lib/screens/home/home.dart
+++ doggy_details/lib/screens/home/home.dart
@@ -1,18 +1,25 @@
 import 'package:doggy_details/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:doggy_details/screens/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: kPrimaryColor,
-          elevation: 0,
-          leading: IconButton(
-            icon: SvgPicture.asset("assets/icons/menu.svg"),
-            onPressed: () {},
-          )),
+      appBar: buildAppBar(),
+      body: body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: kPrimaryColor,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
+        onPressed: () {},
+      ),
     );
   }
 }
