c1d778d5e4c1930c2c63d27860b1b2835cc6310f
tom-kelly25 <107217874+tom-kelly25@users.noreply.github.com>
2023-02-18 16:07:05 +0000

    dev 16:06 18/02

---
+++ doggy_details/lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// Colors that we use in our app
+const kPrimaryColor = Color.fromARGB(255, 46, 13, 212);
+const kTextColor = Color.fromARGB(255, 115, 182, 224);
+const kBackgroundColor = Color(0xFFF9F8FD);
+
+const double kDefaultPadding = 20.0;
--- doggy_details/lib/main.dart
+++ doggy_details/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:doggy_details/constants.dart';
+import 'package:doggy_details/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,20 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Doggy Details',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: kBackgroundColor,
+        primaryColor: kPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: home(),
     );
   }
 }
