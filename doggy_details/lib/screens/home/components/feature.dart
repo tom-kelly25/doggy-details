10a92260f8e79437218542eb0a2a5a8daf188422
tom-kelly25 <107217874+tom-kelly25@users.noreply.github.com>
2023-03-12 13:14:46 +0000

    12/03/2023 13:14

---
+++ doggy_details/lib/screens/home/components/feature.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:doggy_details/constants.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'header.dart';
+import 'titleAndButton.dart';
+
+class featuredCard extends StatelessWidget {
+  const featuredCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      child: Container(
+        margin: EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding * 4,
+        ),
+        width: size.width * 0.8,
+        height: 600,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(
+              "assets/img/lab.png",
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
